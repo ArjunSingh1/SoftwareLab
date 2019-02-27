9702a4c3f1c199f7e8795d6f5f41fcbb16926f8b
bgardner4 <blakeagardner@utexas.edu>
2019-02-27 16:50:47 -0600

    fully integrated with html static pages, links work, images work, waiting for about page

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #import flask framework
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 #import api request for github
 from HttpHandler import get_contributer_statistics
 
@@ -8,14 +8,29 @@ app = Flask(__name__)
 #home page
 @app.route("/")
 @app.route("/home")
-def main():
-	return render_template('Home.html')
+def home():
+	return render_template('home.html')
+
+#consoles page
+@app.route("/consoles")
+def consoles(): 
+	return render_template('consoles.html')
+
+#games page
+@app.route("/games")
+def games(): 
+	return render_template('games.html')
+
+#rating page
+@app.route("/rating")
+def rating(): 
+	return render_template('rating.html')
 
 #about page
 @app.route("/about")
 def about(): 
 	data = get_contributer_statistics()
-	return render_template('About.html', data=data)
+	return render_template('about.html', data=data)
 
-if __name__ == '__name__':
+if __name__ == '__main__':
 	app.run(debug=True)
