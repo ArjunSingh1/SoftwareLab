aee2d0ff4d39d9c8b8e85d5790723b1ee5bede4d
wenranlu <36861841+wenranlu@users.noreply.github.com>
2019-03-30 20:43:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,77 @@
-#import flask framework
-from flask import Flask, render_template, url_for
-#import api request for github
-from HttpHandler import get_contributors_statistics, get_issues_statistics
+from flask import Flask, render_template
+from HttpHandler import get_issues_statistics, get_contributors_statistics
+import csv
 
 app = Flask(__name__)
 
-#home page
 @app.route("/")
+
 @app.route("/home")
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
-#consoles page
 @app.route("/consoles")
-def consoles(): 
-	return render_template('consoles.html')
+def consoles():
+    return render_template('consoles.html')
 
-#hardcode=================================
 @app.route("/consoles/console1")
 def console1():
-    return render_template('console1.html')
+    data = []
+    with open('consoles.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data.append("".join(row))
+    return render_template('console1.html', data=data)
 
 @app.route("/consoles/console2")
 def console2():
-    return render_template('console2.html')
+    data2 = []
+    with open('consoles5.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data2.append("".join(row))
+    return render_template('console2.html', data=data2)
 
 @app.route("/consoles/console3")
 def console3():
-    return render_template('console3.html')
+    data3 = []
+    with open('consoles2.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data3.append("".join(row))
+    return render_template('console3.html', data=data3)
 
 @app.route("/consoles/console4")
 def console4():
-    return render_template('console4.html')
+    data4 = []
+    with open('consoles4.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data4.append("".join(row))
+    return render_template('console4.html', data=data4)
 
 @app.route("/consoles/console5")
 def console5():
-    return render_template('console5.html')
-#==========================================
+    data5 = []
+    with open('consoles3.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data5.append("".join(row))
+    return render_template('console5.html', data=data5)
 
-#games page
+@app.route("/consoles/console6")
+def console6():
+    data6 = []
+    with open('consoles6.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            data6.append("".join(row))
+    return render_template('console6.html', data=data6)
+
 @app.route("/games")
-def games(): 
-	return render_template('games.html')
+def games():
+    return render_template('games.html')
 
-#hardcode=================================
 @app.route("/games/game1")
 def game1():
     return render_template('game1.html')
@@ -63,23 +91,16 @@ def game4():
 @app.route("/games/game5")
 def game5():
     return render_template('game5.html')
-#=========================================
 
-#rating page
-@app.route("/rating")
-def rating(): 
-	return render_template('rating.html')
-
-#about page
 @app.route("/about")
-def about(): 
-	data = get_contributors_statistics()
-	stats = get_issues_statistics()
-	return render_template('about.html', data=data, stats=stats)
+def about():
+    stats = get_issues_statistics()
+    data = get_contributors_statistics()
+    return render_template('about.html', data=data, stats=stats)
 
-@app.errorhandler(500)
-def server_error(e):
-    return 'An internal error occurred.', 500
+@app.route("/rating")
+def rating():
+    return render_template('rating.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
