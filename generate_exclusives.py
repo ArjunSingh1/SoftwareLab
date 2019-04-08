d5a08b8e2bf2e719ae7dd5d73b6253ba2a941242
rabiakhan713 <rabiakhan@utexas.edu>
2019-04-08 17:47:27 -0500

    Modified Exclusive_Games to include the image_link for game title

--- generate_exclusives.py
+++ generate_exclusives.py
@@ -2,25 +2,30 @@ import csv
 #{key : [title, platform]}
 
 dict = {}
-col1 = "title"
-col2 = "platform"
+col1 = 'title'
+col2 = 'image_link'
+col3 = 'platform'
 def main():
-    with open('All_Games') as csvfile:
+    with open('games_lists/All_Games') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             if len(row) == 3:
                 if row[1] != "unreleased":
-                    dict[row[0]] = row[2]
+                    entry = []
+                    entry.append(row[0])
+                    entry.append(row[1])
+                    entry.append(row[2])
+                    dict[row[0]] = entry
         put_dictionary_in_csv()
         print("Done")
 
 
 def put_dictionary_in_csv():
     with open('Exclusive_Games', mode='w') as file:
-        write = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        write.writerow([col1,col2])
+        write = csv.writer(file, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='\\', lineterminator='\n')
+        write.writerow([col1,col2,col3])
         for key, value in dict.items():
-            write.writerow([key,value])
+            write.writerow(value)
 
 
 if __name__ == "__main__":
